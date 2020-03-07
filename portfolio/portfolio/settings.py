a1762c3024d58ec0dc4e0fff22b34a71f0da72ce
Shengda <39946188+shengdamao@users.noreply.github.com>
2020-03-07 21:11:41 +0800

    models-1
    models-1

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'job.apps.JobConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # (added myself, need check docs)
+
+MEDIA_URL = '/media/'                           # (added myself, need check docs)
